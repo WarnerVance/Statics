03f44e3067c99b051f3bc084cfd1099bd561ea04
Warner Vance <warnervance@icloud.com>
2025-02-17 16:20:53 -0600

    Updated Docstrings in vector.py
    Signed-off-by: Warner Vance <warnervance@icloud.com>

--- geom3d/vector.py
+++ geom3d/vector.py
@@ -36,8 +36,10 @@ class Vector:
     @property
     def norm(self):
         """
-        Finds the magnitude of the given vector using the pythagorean theorem.
-        :return: float
+        Calculate and return the Euclidean norm (magnitude) of a vector.
+        The norm is computed as the square root of the sum of the squared components.
+        :return: The Euclidean norm of the vector.
+        :rtype: float
         """
         return math.sqrt(self.i ** 2 + self.j ** 2 + self.k ** 2)
 
@@ -46,9 +48,18 @@ class Vector:
 
     def scaled_by(self, factor):
         """
-        Multiplies the elements in the vector by the given factor.
-        :param factor: a float or int
-        :return: Vector
+        Scales the vector by a given factor.
+
+        This method multiplies each component of the vector
+        (i, j, k) by the specified factor to produce a scaled
+        vector. It returns a new Vector instance with scaled
+        components.
+
+        :param factor: The factor by which to scale the vector.
+        :type factor: float or int
+        :return: A new Vector instance with components scaled
+            by the given factor.
+        :rtype: Vector
         """
         return Vector(
             self.i * factor,
@@ -61,17 +72,35 @@ class Vector:
 
     def dot(self, other):
         """
-        Returns the dot product of two vectors
-        :param other: Vector
-        :return: int or float
+        Computes the dot product of this vector with another vector.
+
+        The dot product is a scalar value that is the sum of the products
+        of the corresponding components of the two vectors. This operation
+        takes two 3-dimensional vectors (self and other) and computes their
+        dot product.
+
+        :param other: Another vector with which the dot product is to be
+            computed.
+        :type other: Vector
+        :return: The dot product of the two vectors. It represents the
+            scalar value resulting from the dot product calculation.
+        :rtype: float
         """
         return self.i * other.i + self.j * other.j + self.k * other.k
 
     def cross(self, other):
         """
-        Returns the cross product of two vectors.
-        :param other: Vector
-        :return: Vector
+        Computes the cross product of the current vector with another vector.
+
+        The cross product is performed between two 3D vectors and results in a
+        new vector that is perpendicular to both input vectors. This is often
+        used in physics, computer graphics, and vector mathematics to determine
+        the orientation of two vectors in three-dimensional space.
+
+        :param other: The other vector to compute the cross product with.
+        :type other: Vector
+        :return: A new vector that is the result of the cross product operation.
+        :rtype: Vector
         """
         return Vector(
             self.j * other.k - self.k * other.j,
@@ -82,8 +111,14 @@ class Vector:
     @property
     def unit(self):
         """
-        Finds the unit vector of the given vector.
-        :return: Vector
+        Computes the unit (normalized) vector of the current vector.
+
+        If the norm of the vector is close to zero, it returns the vector itself to avoid
+        division by zero. Otherwise, it calculates the unit vector by scaling the current
+        vector using the inverse of its norm.
+
+        :rtype: Vector
+        :return: The unit vector if the norm is not zero, or the vector itself if the norm is zero.
         """
         if are_close_enough(self.norm, 0):
             # This allows for the special case were we try to find the unit vector of the
